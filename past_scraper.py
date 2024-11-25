9de873dbfa68fd29ec73323ccb86df0870488652
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-11-24 22:22:36 -0500

    Implemented try catch to counter any failed scrapes

--- past_scraper.py
+++ past_scraper.py
@@ -30,25 +30,28 @@ dropdown = driver.find_element(By.CSS_SELECTOR, "select.DropDown_select__4pIg9")
 
 previous_text = ""
 for i in range(29, 1, -1):
-    dropdown.click()
-    driver.implicitly_wait(10)   
-    season = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/div[3]/section[1]/div/div/div[1]/label/div/select/option[" + str(i) + "]")   
-    season.click()
-    dropdown.click()
-    
-    
-    WebDriverWait(driver, 20).until(
-        lambda d: previous_text != d.find_element(By.CSS_SELECTOR, "tbody.Crom_body__UYOcU").text
-    )
-    
-    stats = driver.find_element(By.CSS_SELECTOR, "tbody.Crom_body__UYOcU")
-    previous_text = stats.text
-    stats_formatted = stats.text.split("\n")
-    for j in range(len(stats_formatted)):
-        print(stats_formatted[j])
-    
-    print()
-    
-    
-    
+    for retry in range(3):
+        try:
+            dropdown.click()
+            driver.implicitly_wait(10)   
+            season = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/div[3]/section[1]/div/div/div[1]/label/div/select/option[" + str(i) + "]")   
+            season.click()
+            dropdown.click()
+            
+            
+            WebDriverWait(driver, 20).until(
+                lambda d: previous_text != d.find_element(By.CSS_SELECTOR, "tbody.Crom_body__UYOcU").text
+            )
+            
+            stats = driver.find_element(By.CSS_SELECTOR, "tbody.Crom_body__UYOcU")
+            previous_text = stats.text
+            stats_formatted = stats.text.split("\n")
+            for j in range(len(stats_formatted)):
+                print(stats_formatted[j])
+            
+            print()
+            break
+        except Exception as e:
+            print(f"Retrying for {season.text} due to error: {e}")
+
 driver.quit()
